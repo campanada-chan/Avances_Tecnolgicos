6c5cf8d681f6956dd366432c7f86939abba941d7
campanada-chan <campanadaeminor28no4@gmail.com>
2025-10-14 10:46:44 -0400

    Update app.py
    agregar informacion

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ def chatbot_response(user_message):
     user_message = user_message.lower().strip()
     if "hola" in user_message or "buenos dias" in user_message:
         return "¡Hola! ¿Cómo puedo ayudarte hoy?"
+    if "¿que puedes hacer?" in user_message or "¿que hace esto?" in user_message:
+        return "Proporciono informacion, relacionada a la empresa [nombre de la empresa], ¿Necesitas ayuda con algo màs?"
+    elif "Correo" in user_message or "Email" in user_message:
+        return "Nuestro correo oficial es: ejemplocorreo@gmail.com"
     elif "cómo estás" in user_message or "¿qué tal?" in user_message:
         return "Estoy bien, gracias por preguntar. ¿Y tú?"
     elif "adiós" in user_message or "hasta luego" in user_message:
@@ -15,12 +19,14 @@ def chatbot_response(user_message):
     elif "horario" in user_message:
         return "Nuestro horario de atención es de 9:00 a 18:00 de lunes a viernes."
     elif "contacto" in user_message:
-        return "Puedes contactarnos en el 555-1234 o enviarnos un correo a info@ejemplo.com."
+        return "Puedes contactarnos en el 555-1234"
     elif "ubicación" in user_message:
         return "Estamos ubicados en la Calle 123."
+    elif "info" in user_message:
+        return "Somos una empresa dedicada a la creacion especializada de [producto], este es nuestro chatbot que responde preguntas basicas, mas informacion al +00 12345678"
     else:
         return "Lo siento, no entiendo tu pregunta. ¿Puedes reformularla?"
-    
+        
 @app.route('/')
 def index():
     return render_template('index.html')
